25a7af434f5f6bca3b047f90dbfd89829c8df94b
Olaf Lüke <olaf@tinkerforge.com>
2016-11-25 15:27:47 +0100

    Return out of state machine if we know there is not enough data

--- software/src/bootloader_spitfp.c
+++ software/src/bootloader_spitfp.c
@@ -313,6 +313,10 @@ void spitfp_tick(BootloaderStatus *bootloader_status) {
 				}
 
 				data_length = data;
+				if((start+used - i) < data_length) {
+					// There can't be enough data for a whole message, we can return here.
+					return;
+				}
 				PEARSON(checksum, data_length);
 
 				break;
